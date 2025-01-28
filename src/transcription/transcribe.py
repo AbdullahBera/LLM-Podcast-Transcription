bc54b902a890ce5f53b3cf1610dc192f8e045a2b
AbdullahBera <kucukabdullahbera@gmail.com>
2025-01-27 21:46:14 -0800

    changes

--- src/transcription/transcribe.py
+++ src/transcription/transcribe.py
@@ -69,6 +69,6 @@ class AudioTranscriber:
 
         return output_file
 
-input_file = "/Users/bera/Desktop/projects/LLM-Podcast-Transcription/data/audio/li_lu.mp3"
-transcriber = AudioTranscriber(model_name="base")
-result = transcriber.process_audio(input_file)
+# input_file = "/Users/bera/Desktop/projects/LLM-Podcast-Transcription/data/audio/li_lu.mp3"
+# transcriber = AudioTranscriber(model_name="base")
+# result = transcriber.process_audio(input_file)
